61ec750d45dcd19b020fd0049bf198a35407a8d4
justinkrojos <justinkrojos@gmail.com>
2020-06-03 13:38:15 +1200

    JR added chat and due date to properties column

--- src/index.js
+++ src/index.js
@@ -71,3 +71,18 @@ function doShare(fileName) {
 
 var openTab = document.getElementById("firstTab");
 openTab.click();
+
+function sendMsg() {
+  document.getElementById("myMsgBox").style.display = "block";
+  var text1 = document.getElementById("textarea").value;
+  document.getElementById("msgContent").innerHtml = text1;
+}
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
