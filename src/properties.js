d169b669b197cf965842affdaea268bab96c39eb
justinkrojos <justinkrojos@gmail.com>
2020-06-05 16:08:33 +1200

    JR added dynamic file-property changes

--- src/properties.js
+++ src/properties.js
@@ -19,17 +19,35 @@ function sendMsg() {
     document.getElementById("msgContent").innerHtml = text1;
   }
   
-  function openNav() {
-    if (document.getElementById("mySidenav").style.width === "300px") {
+  function openNav(a) {
+    if (document.getElementById("mySidenav").style.width === "300px" && document.getElementById("file-name").innerHTML === a.getElementsByTagName("p")[0].innerHTML) {
       closeNav();
     }
     else {
+      document.getElementById("file-name").innerHTML = a.getElementsByTagName("p")[0].innerHTML;
       document.getElementById("mySidenav").style.width = "300px";
       document.getElementById("userAvatar").style.marginRight = "300px";
       document.getElementById("myInput").style.minWidth = "800px";
+      if (a.getElementsByTagName("p")[0].innerHTML === "File 1") {
+        setTimeout("showMsg1()", 100);
+        setTimeout("showMsg2()", 500);
+      }
+      else {
+        document.getElementById("msgcontainer1").style.display = "none";
+        document.getElementById("msgcontainer2").style.display = "none";
+        document.getElementById("myMsgBox").style.display = "none";
+      }
     }
-   
   }
+
+  function showMsg1() {
+    document.getElementById("msgcontainer1").style.display = "block";
+  }
+
+  function showMsg2() {
+    document.getElementById("msgcontainer2").style.display = "block";
+  }
+  
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
